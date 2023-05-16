546a4fb70bddbd660efb2e81dea5dd16ecc57140
nohimgodage <nohim.godage@gmail.com>
2023-05-16 08:05:40 +0530

    Adding Route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import { graphqlHTTP } from "express-graphql"
 
 const PORT = 5001;
 
 const app = express();
+
+//Register serving route
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}));
+
+// Run Server
 app.listen(PORT, () => {
     console.log(`Server running on: ${PORT}`);
 });
