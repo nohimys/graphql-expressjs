58c588f1104199296834bd0e6f8fbccab60e2b4b
nohimgodage <nohim.godage@gmail.com>
2023-05-16 20:42:43 +0530

    Hello World Basic Query

--- src/server.js
+++ src/server.js
@@ -1,12 +1,37 @@
 import express from 'express'
 import { graphqlHTTP } from "express-graphql"
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString
+} from 'graphql'
 
 const PORT = 5001;
 
 const app = express();
 
+
+//How to query in the browser for the below thing
+// {
+//     message
+// }
+
+//Define Schema
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name:'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+});
+
 //Register serving route
 app.use('/graphql', graphqlHTTP({
+    schema,
     graphiql: true
 }));
 
